bdbcf06c1291f0476803eebd31ef2aff215ee45c
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-14 14:57:02 +0200

    personas

---
+++ src/data/mock/data/vedtak.ts
@@ -0,0 +1,10 @@
+import { RSVedtakWrapper } from '../../../types/rs-types/rs-vedtak'
+
+export const vedtakMed100Grad: RSVedtakWrapper = {
+    id: 'dff11217-31ea-404a-86ab-b521a6a946af',
+    lest: true,
+    lestDato: '1970-01-01T01:00:00+01:00',
+    opprettet: '2021-05-06',
+    annullert: false
+}
+
--- src/data/mock/index.ts
+++ src/data/mock/index.ts
@@ -1,9 +1,7 @@
 import FetchMock, { MiddlewareUtils } from 'yet-another-fetch-mock'
 
-import { Sykmelding } from '../../types/sykmelding'
 import env from '../../utils/environment'
-import { nyeVedtak } from './data/rs-vedtak'
-import { soknader } from './data/soknader'
+import { defaultPersona, heltFrisk, Persona } from './data/personas'
 
 const mock = FetchMock.configure({
     enableFallback: true,
@@ -12,24 +10,23 @@ const mock = FetchMock.configure({
     )
 })
 
-mock.get(`${env.flexGatewayRoot}/syfosoknad/api/soknader`,
-    (req, res, ctx) => res(ctx.json(soknader)))
 
-mock.get(`${env.flexGatewayRoot}/spinnsyn-backend/api/v2/vedtak`,
-    (req, res, ctx) => res(ctx.json(nyeVedtak)))
+function setUpMock(persona: Persona) {
+    mock.get(`${env.flexGatewayRoot}/syfosoknad/api/soknader`,
+        (req, res, ctx) => res(ctx.json(persona.soknader)))
 
-mock.post(`${env.flexGatewayRoot}/spinnsyn-backend/api/v2/vedtak/:id/les`, () => Promise.resolve({ status: 200 }))
+    mock.get(`${env.flexGatewayRoot}/spinnsyn-backend/api/v2/vedtak`,
+        (req, res, ctx) => res(ctx.json(persona.vedtak)))
 
 
-const sykmeldinger: Sykmelding[] = [ {
-    id: 'APEN',
-    sykmeldingStatus: { statusEvent: 'APEN' },
-    behandlingsutfall: { status: 'OK' }
-}, {
-    id: 'APEN',
-    sykmeldingStatus: { statusEvent: 'APEN' },
-    behandlingsutfall: { status: 'INVALID' }
-} ]
+    mock.get(`${env.sykmeldingerBackendProxyRoot}/api/v1/sykmeldinger`,
+        (req, res, ctx) => res(ctx.json(persona.sykmeldinger)))
+}
 
-mock.get(`${env.sykmeldingerBackendProxyRoot}/api/v1/sykmeldinger`,
-    (req, res, ctx) => res(ctx.json(sykmeldinger)))
+
+const href = window.location.href
+if (href.includes('helt-frisk')) {
+    setUpMock(heltFrisk)
+} else {
+    setUpMock(defaultPersona)
+}
