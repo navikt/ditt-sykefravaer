bc2a5628655ea6546580474afb865189bb913def
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-15 15:12:46 +0200

    Fiks knekt tes

--- src/components/banner/banner.test.ts
+++ src/components/banner/banner.test.ts
@@ -3,6 +3,6 @@ import 'dayjs/locale/nb'
 import { tekst } from '../../utils/tekster'
 
 it('Returns text from bundle', () => {
-    const text = tekst('spinnsyn.sidetittel.liste')
+    const text = tekst('sidetittel.liste')
     expect(text).toEqual('Ditt sykefravær')
 })
