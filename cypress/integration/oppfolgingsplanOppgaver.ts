447825ee68fe9e54345c56b3e23a9f261e3ab255
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-23 16:17:06 +0200

    Siste tester (#21)

--- cypress/integration/oppfolgingsplanOppgaver.ts
+++ cypress/integration/oppfolgingsplanOppgaver.ts
@@ -1,7 +1,22 @@
 describe('Tester oppfølgingsplan oppgaver', () => {
 
-    it('En oppgave sammen', () => {
-        cy.visit('http://localhost:8080/syk/sykefravaer?testperson=en-oppfolgingsplan-til-godkjenning')
+    it('En ny oppgave', () => {
+        cy.visit('http://localhost:8080/syk/sykefravaer?testperson=en-ny-oppfolgingsplan')
         cy.get('.oppgaver > .alertstripe').contains('Lederen din har begynt på oppfølgingsplanen dere skal skrive sammen')
+    })
+
+    it('To nye oppgaver', () => {
+        cy.visit('http://localhost:8080/syk/sykefravaer?testperson=to-nye-oppfolgingsplaner')
+        cy.get('.oppgaver > .alertstripe').contains('Det er startet to oppfølgingsplaner du er en del av')
+    })
+
+    it('En til godkjenning', () => {
+        cy.visit('http://localhost:8080/syk/sykefravaer?testperson=en-ny-oppfolgingsplan-til-godkjenning')
+        cy.get('.oppgaver > .alertstripe').contains('Du har en oppfølgingsplan som venter på godkjenning av deg')
+    })
+
+    it('To til godkjenning', () => {
+        cy.visit('http://localhost:8080/syk/sykefravaer?testperson=to-nye-oppfolgingsplaner-til-godkjenning')
+        cy.get('.oppgaver > .alertstripe').contains('Du har to oppfølgingsplaner som venter på godkjenning av deg')
     })
 })
--- src/data/mock/data/oppfolginsplanTestPersoner.ts
+++ src/data/mock/data/oppfolginsplanTestPersoner.ts
@@ -28,7 +28,7 @@ const commonPersona: Persona = {
     arbeidsrettetOppfolging: { underOppfolging: false },
 }
 
-export const enNyTilGodkjenning = () => {
+export const enNyOppfolgingsplan = () => {
     const ret = jsonDeepCopy(commonPersona)
     ret.oppfolgingsplaner = [ {
         'id': 4183,
@@ -47,3 +47,112 @@ export const enNyTilGodkjenning = () => {
     return ret
 }
 
+
+export const toNyeOppfolgingsplaner = () => {
+    const ret = jsonDeepCopy(commonPersona)
+    ret.oppfolgingsplaner = [ {
+        'id': 4183,
+        'status': 'UNDER_ARBEID',
+        'virksomhet': { 'virksomhetsnummer': '972674818' },
+        'godkjenninger': [],
+        'arbeidstaker': {
+            'fnr': '18048600146',
+            'sistInnlogget': null,
+
+        },
+        'sistEndretAv': {
+            'fnr': '13098023954',
+        }
+    }, {
+        'id': 4184,
+        'status': 'UNDER_ARBEID',
+        'virksomhet': { 'virksomhetsnummer': '972674818' },
+        'godkjenninger': [],
+        'arbeidstaker': {
+            'fnr': '18048600146',
+            'sistInnlogget': null,
+
+        },
+        'sistEndretAv': {
+            'fnr': '13098023954',
+        }
+    } ]
+    return ret
+}
+
+
+export const enNyTilGodkjenning = () => {
+    const ret = jsonDeepCopy(commonPersona)
+    ret.oppfolgingsplaner = [ {
+        'id': 4183,
+        'status': 'UNDER_ARBEID',
+        'virksomhet': { 'virksomhetsnummer': '972674818' },
+        'godkjenninger': [
+            {
+                godkjenningsTidspunkt: '2021-05-31T12:12:08.038038',
+                godkjent: true,
+                godkjentAv: {
+                    fnr: '13098023954'
+                }
+            }
+        ],
+        'arbeidstaker': {
+            'fnr': '18048600146',
+            'sistInnlogget': null,
+
+        },
+        'sistEndretAv': {
+            'fnr': '13098023954',
+        }
+    } ]
+    return ret
+}
+
+
+export const toTilGodkjenning = () => {
+    const ret = jsonDeepCopy(commonPersona)
+    ret.oppfolgingsplaner = [ {
+        'id': 4183,
+        'status': 'UNDER_ARBEID',
+        'virksomhet': { 'virksomhetsnummer': '972674818' },
+        'godkjenninger': [
+            {
+                godkjenningsTidspunkt: '2021-05-31T12:12:08.038038',
+                godkjent: true,
+                godkjentAv: {
+                    fnr: '13098023954'
+                }
+            }
+        ],
+        'arbeidstaker': {
+            'fnr': '18048600146',
+            'sistInnlogget': null,
+
+        },
+        'sistEndretAv': {
+            'fnr': '13098023954',
+        }
+    }, {
+        'id': 4184,
+        'status': 'UNDER_ARBEID',
+        'virksomhet': { 'virksomhetsnummer': '972674818' },
+        'godkjenninger': [
+            {
+                godkjenningsTidspunkt: '2021-05-31T12:12:08.038038',
+                godkjent: true,
+                godkjentAv: {
+                    fnr: '13098023954'
+                }
+            }
+        ],
+        'arbeidstaker': {
+            'fnr': '18048600146',
+            'sistInnlogget': null,
+
+        },
+        'sistEndretAv': {
+            'fnr': '13098023954',
+        }
+    } ]
+    return ret
+}
