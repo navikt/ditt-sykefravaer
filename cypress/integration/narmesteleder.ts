a69bede89d7752764b1348c279d5513f2f1d7c8a
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2021-06-29 09:25:25 +0200

    Avkreft nl med useMutation (#24)
    * Bruker useMutation for å avkrefte nl
    * Større cypress vindu

---
+++ cypress/integration/narmesteleder.ts
@@ -0,0 +1,23 @@
+
+describe('Tester narmesteledere', () => {
+
+    it('Har narmesteleder og kan avkrefte den', () => {
+        cy.visit('http://localhost:8080/syk/sykefravaer?testperson=langtidssykmeldt')
+
+        cy.get('.arbeidssituasjon-panel .situasjon')
+            .should('contain', 'Ansatt i Sykmeldingsperioder AS')
+            .should('contain', 'Din nærmeste leder er Albus Dumbledore')
+            .should('contain', 'Arbeidsgiveren din betaler lønn også etter de 16 første dagene.')
+            .contains('Meld fra om endring')
+            .click()
+
+        cy.get('.modal')
+            .should('contain', 'Endre nærmeste leder')
+            .contains('Ja, jeg er sikker')
+            .click()
+
+        cy.get('.arbeidssituasjon-panel .situasjon')
+            .should('contain', 'Ansatt i Sykmeldingsperioder AS')
+            .should('not.contain', 'Din nærmeste leder')
+    })
+})
