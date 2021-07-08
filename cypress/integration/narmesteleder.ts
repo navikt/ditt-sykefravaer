0944a3c70a7af2feb44e3fda3125e4e4e8e86696
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-07-08 11:33:49 +0200

    Fix test

--- cypress/integration/narmesteleder.ts
+++ cypress/integration/narmesteleder.ts
@@ -4,7 +4,7 @@ describe('Tester narmesteledere', () => {
     it('Har narmesteleder og kan avkrefte den', () => {
         cy.visit('http://localhost:8080/syk/sykefravaer?testperson=langtidssykmeldt')
 
-        cy.get('.arbeidssituasjon-panel .situasjon')
+        cy.get('.arbeidssituasjon-panel .situasjon__panel')
             .should('contain', 'Ansatt i Sykmeldingsperioder AS')
             .should('contain', 'Din nærmeste leder er Albus Dumbledore')
             .should('contain', 'Arbeidsgiveren din betaler lønn også etter de 16 første dagene.')
@@ -16,7 +16,7 @@ describe('Tester narmesteledere', () => {
             .contains('Ja, jeg er sikker')
             .click()
 
-        cy.get('.arbeidssituasjon-panel .situasjon')
+        cy.get('.arbeidssituasjon-panel .situasjon__panel')
             .should('contain', 'Ansatt i Sykmeldingsperioder AS')
             .should('not.contain', 'Din nærmeste leder')
     })
