3d0a41672f92ce305f77db74b69a7d8b51328179
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2021-07-20 09:27:27 +0200

    Unngå feil ved fjerning av flere klassenavn

--- src/utils/setBodyClass.ts
+++ src/utils/setBodyClass.ts
@@ -1,7 +1,6 @@
-
 export default (name: string) => {
     if (document.body.className !== '') {
-        document.body.classList.remove(document.body.className)
+        document.body.setAttribute('class', '')
     }
     document.body.classList.add(name)
 }
