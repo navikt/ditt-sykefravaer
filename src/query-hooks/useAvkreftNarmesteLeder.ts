f6392a7b2d6c20fbb86fa07a646d1bad144adc87
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2021-07-22 11:25:01 +0200

    Fjernet alle avhengigheter som ikke er våre

--- src/query-hooks/useAvkreftNarmesteLeder.ts
+++ src/query-hooks/useAvkreftNarmesteLeder.ts
@@ -6,16 +6,17 @@ import Fetch from '../utils/fetch'
 export default function(org: string) {
     const queryClient = useQueryClient()
 
-    return useMutation<unknown, Error>(() => {
-        return Fetch.authenticatedPost(
-            `${env.narmestelederUrl}/${org}/avkreft`
-        )
-    },
-    {
-        onSuccess: () => {
-            queryClient.invalidateQueries('narmesteledere')
+    return useMutation<unknown, Error>(
+        () => {
+            return Fetch.authenticatedPost(
+                `${env.narmestelederUrl}/${org}/avkreft`
+            )
+        },
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries('narmesteledere')
+            }
         }
-    }
     )
 }
 
