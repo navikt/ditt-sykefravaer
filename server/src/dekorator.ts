d57973ca8f2f82b781bed96027301f9b8f96aef3
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-09-28 13:20:52 +0200

    Bruker ikke simple dekorator

--- server/src/dekorator.ts
+++ server/src/dekorator.ts
@@ -2,11 +2,10 @@ import { injectDecoratorServerSide } from '@navikt/nav-dekoratoren-moduler/ssr'
 
 export const getHtmlWithDecorator = (filePath) =>
     injectDecoratorServerSide({
+        dekoratorenUrl: process.env.DECORATOR_URL,
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         env: process.env.DECORATOR_ENV as any,
-        dekoratorenUrl: process.env.DECORATOR_URL,
         filePath: filePath,
-        simple: true,
         chatbot: false,
         feedback: false,
         urlLookupTable: false,
