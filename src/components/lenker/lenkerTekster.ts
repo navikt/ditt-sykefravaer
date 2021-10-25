c18bd6d96f565ad04525555a573025b736c81169
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-25 12:43:32 +0200

    Svar på søknader

--- src/components/lenker/lenkerTekster.ts
+++ src/components/lenker/lenkerTekster.ts
@@ -2,7 +2,7 @@ export const LenkerTekster = {
     'lenker.soknader': 'Søknader',
     'lenker.soknader.undertekst': 'fra deg',
     'lenker.sykmelding': 'Sykmeldinger',
-    'lenker.utbetaling': 'Behandlede søknader',
+    'lenker.utbetaling': 'Svar på søknader',
     'lenker.utbetaling.undertekst': 'fra NAV',
     'lenker.aktivitetsplan': 'Aktivitetsplan',
     'lenker.aktivitetsplan.undertekst': 'for deg og NAV',
