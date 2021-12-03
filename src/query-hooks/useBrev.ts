48a0036af1bb9829869ebea8586c446bd3f92521
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-12-03 11:06:20 +0100

    Remove proxy and chage to flex-gateway

--- src/query-hooks/useBrev.ts
+++ src/query-hooks/useBrev.ts
@@ -7,7 +7,7 @@ import Fetch from '../utils/fetch'
 export default function() {
     return useQuery<Brev[], Error>('brev', () =>
         Fetch.authenticatedGet(
-            `${env.loginServiceRedirectUrl()}/api/v1/arbeidstaker/brev`,
+            `${env.flexGatewayRoot()}/api/v1/arbeidstaker/brev`,
             async(data) => {
                 return data as Brev[]
             },
