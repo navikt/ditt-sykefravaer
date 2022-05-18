9550651ed85d3fd0c822d396214356701bb4bc92
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-05-18 12:43:35 +0200

    Sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withLess = require('next-with-less')
+const { withSentryConfig } = require('@sentry/nextjs')
+const withPlugins = require('next-compose-plugins')
 
 const csp = {
     'default-src': ["'none'"],
@@ -45,54 +47,67 @@ const cspHeader = [
     },
 ]
 
-module.exports = withLess({
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: cspHeader,
-            },
-        ]
-    },
-    basePath: '/syk/sykefravaer',
-    lessLoaderOptions: {},
-    assetPrefix: process.env.ASSET_PREFIX || '',
-    generateEtags: false, //Disabler etag i pages
-    serverRuntimeConfig: {
-        // Will only be available on the server side
-        decoratorEnv: process.env.DECORATOR_ENV,
-        decoratorUrl: process.env.DECORATOR_URL,
-        noDecorator: process.env.NO_DECORATOR,
-    },
-    publicRuntimeConfig: {
-        // Will be available on both server and client
-        loginserviceUrl: process.env.LOGINSERVICE_URL,
-        loginServiceRedirectUrl: process.env.LOGINSERVICE_REDIRECT_URL,
-        loginserviceIdportenDiscoveryUrl:
-            process.env.LOGINSERVICE_IDPORTEN_DISCOVERY_URL,
-        loginserviceIdportenAudience:
-            process.env.LOGINSERVICE_IDPORTEN_AUDIENCE,
-        flexGatewayRoot: process.env.FLEX_GATEWAY_ROOT,
-        mockBackend: process.env.MOCK_BACKEND,
-        opplaering: process.env.OPPLAERING,
-        sykefravaerUrl: process.env.SYKEFRAVAER_URL,
-        dittNavUrl: process.env.DITTNAV_URL,
-        amplitudeKey: process.env.AMPLITUDE_KEY,
-        amplitudeEnabled: process.env.AMPLITUDE_ENABLED,
-        environment: process.env.ENVIRONMENT,
-        spinnsynFrontendInterne: process.env.SPINNSYN_FRONTEND_INTERNE,
-        sykmeldingerBackendProxyRoot:
-            process.env.SYKMELDINGER_BACKEND_PROXY_ROOT,
-        syfoApiRoot: process.env.SYFOAPI_ROOT,
-        sykepengesoknadUrl: process.env.SYKEPENGESOKNAD_URL,
-        spinnsynUrl: process.env.SPINNSYN_URL,
-        sykmeldingUrl: process.env.SYKMELDING_URL,
-        aktivitetsplanUrl: process.env.AKTIVITETSPLAN_URL,
-        oppfolgingsplanUrl: process.env.OPPFOLGINGSPLAN_URL,
-        dialogmoteUrl: process.env.DIALOGMOTE_URL,
-        newDialogmoteUrl: process.env.NEW_DIALOGMOTE_URL,
-        narmestelederUrl: process.env.NARMESTELEDER_URL,
-        backendSoknadApp: process.env.BACKEND_SOKNAD_APP,
-        snartSluttUrl: process.env.SNART_SLUTT_URL,
-    },
-})
+module.exports = withPlugins(
+    [
+        [withLess],
+        [
+            (nextConfig) =>
+                process.env.ENABLE_SENTRY
+                    ? withSentryConfig(nextConfig, {
+                          silent: true,
+                      })
+                    : nextConfig,
+        ],
+    ],
+    {
+        async headers() {
+            return [
+                {
+                    source: '/:path*',
+                    headers: cspHeader,
+                },
+            ]
+        },
+        basePath: '/syk/sykefravaer',
+        lessLoaderOptions: {},
+        assetPrefix: process.env.ASSET_PREFIX || '',
+        generateEtags: false, //Disabler etag i pages
+        serverRuntimeConfig: {
+            // Will only be available on the server side
+            decoratorEnv: process.env.DECORATOR_ENV,
+            decoratorUrl: process.env.DECORATOR_URL,
+            noDecorator: process.env.NO_DECORATOR,
+        },
+        publicRuntimeConfig: {
+            // Will be available on both server and client
+            loginserviceUrl: process.env.LOGINSERVICE_URL,
+            loginServiceRedirectUrl: process.env.LOGINSERVICE_REDIRECT_URL,
+            loginserviceIdportenDiscoveryUrl:
+                process.env.LOGINSERVICE_IDPORTEN_DISCOVERY_URL,
+            loginserviceIdportenAudience:
+                process.env.LOGINSERVICE_IDPORTEN_AUDIENCE,
+            flexGatewayRoot: process.env.FLEX_GATEWAY_ROOT,
+            mockBackend: process.env.MOCK_BACKEND,
+            opplaering: process.env.OPPLAERING,
+            sykefravaerUrl: process.env.SYKEFRAVAER_URL,
+            dittNavUrl: process.env.DITTNAV_URL,
+            amplitudeKey: process.env.AMPLITUDE_KEY,
+            amplitudeEnabled: process.env.AMPLITUDE_ENABLED,
+            environment: process.env.ENVIRONMENT,
+            spinnsynFrontendInterne: process.env.SPINNSYN_FRONTEND_INTERNE,
+            sykmeldingerBackendProxyRoot:
+                process.env.SYKMELDINGER_BACKEND_PROXY_ROOT,
+            syfoApiRoot: process.env.SYFOAPI_ROOT,
+            sykepengesoknadUrl: process.env.SYKEPENGESOKNAD_URL,
+            spinnsynUrl: process.env.SPINNSYN_URL,
+            sykmeldingUrl: process.env.SYKMELDING_URL,
+            aktivitetsplanUrl: process.env.AKTIVITETSPLAN_URL,
+            oppfolgingsplanUrl: process.env.OPPFOLGINGSPLAN_URL,
+            dialogmoteUrl: process.env.DIALOGMOTE_URL,
+            newDialogmoteUrl: process.env.NEW_DIALOGMOTE_URL,
+            narmestelederUrl: process.env.NARMESTELEDER_URL,
+            backendSoknadApp: process.env.BACKEND_SOKNAD_APP,
+            snartSluttUrl: process.env.SNART_SLUTT_URL,
+        },
+    }
+)
