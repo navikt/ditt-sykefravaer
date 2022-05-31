5cd38df73cbb8992606bba006382c31b204b8a3e
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2022-05-31 15:25:27 +0200

    Lagt tilbake test på isMockBackend()

--- src/components/oppgaver/inntektsmeldingBeskjed.ts
+++ src/components/oppgaver/inntektsmeldingBeskjed.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
 
+import { isMockBackend } from '../../utils/environment'
 import { tekst } from '../../utils/tekster'
 import { Oppgave } from './oppgaveTyper'
 
@@ -8,22 +9,24 @@ export const skapInntektsmeldingOppgave = (lenke: string): Oppgave[] => {
     // TODO: logikk for visning av inntektsmeldingoppgave
     // TODO: innhenting av arbeidsgiver og startdato for sykefravær
 
-    oppgaver.push({
-        type: 'info',
-        tekst: tekst('oppgaver.inntektsmelding.mangler', {
-            '%ARBEIDSGIVER%': 'Test Arbeidsgiver AS',
-            '%STARTDATO%': dayjs(new Date()).format('D. MMMM YYYY'),
-        }),
-        lenke,
-    })
-    oppgaver.push({
-        type: 'success',
-        tekst: tekst('oppgaver.inntektsmelding', {
-            '%ARBEIDSGIVER%': 'Test Arbeidsgiver AS',
-            '%STARTDATO%': dayjs(new Date()).format('D. MMMM YYYY'),
-        }),
-        lenke: '#',
-    })
+    if (isMockBackend()) {
+        oppgaver.push({
+            type: 'info',
+            tekst: tekst('oppgaver.inntektsmelding.mangler', {
+                '%ARBEIDSGIVER%': 'Test Arbeidsgiver AS',
+                '%STARTDATO%': dayjs(new Date()).format('D. MMMM YYYY'),
+            }),
+            lenke,
+        })
+        oppgaver.push({
+            type: 'success',
+            tekst: tekst('oppgaver.inntektsmelding', {
+                '%ARBEIDSGIVER%': 'Test Arbeidsgiver AS',
+                '%STARTDATO%': dayjs(new Date()).format('D. MMMM YYYY'),
+            }),
+            lenke: '#',
+        })
+    }
 
     return oppgaver
 }
