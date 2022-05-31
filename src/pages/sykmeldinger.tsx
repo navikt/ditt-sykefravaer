7bec9eec5f94fd35d2dd5f2b4d08196c135a43b7
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-05-31 14:53:01 +0200

    Disabler statisk rendring

--- src/pages/sykmeldinger.tsx
+++ src/pages/sykmeldinger.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next'
 import React from 'react'
 
 import { Redirect } from '../components/redirect/redirect'
@@ -5,6 +6,13 @@ import { sykmeldingUrl } from '../utils/environment'
 
 export const RedirectSykmeldinger = () => {
     return <Redirect addresse={`${sykmeldingUrl()}`} />
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    // Disable static rendring
+    return {
+        props: {},
+    }
 }
 
 export default RedirectSykmeldinger
