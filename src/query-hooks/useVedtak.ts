9b5b4549d30bf2756f0f3dc51af870e25db457b2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-06-07 10:00:36 +0200

    Hent vedtak med token x

--- src/query-hooks/useVedtak.ts
+++ src/query-hooks/useVedtak.ts
@@ -1,13 +1,12 @@
 import { useQuery } from 'react-query'
 
 import { RSVedtakWrapper } from '../types/vedtak'
-import { flexGatewayRoot } from '../utils/environment'
 import Fetch from '../utils/fetch'
 
 export default function UseVedtak() {
     return useQuery<RSVedtakWrapper[], Error>('vedtak', () =>
         Fetch.authenticatedGet(
-            `${flexGatewayRoot()}/spinnsyn-backend/api/v2/vedtak`,
+            '/syk/sykefravaer/api/v1/vedtak',
             async (data) => {
                 return data as RSVedtakWrapper[]
             }
