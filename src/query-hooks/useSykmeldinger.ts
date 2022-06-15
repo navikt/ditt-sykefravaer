ec5de9f2be0d413a6b30a25d6a2422c98f8a33d4
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-06-15 11:14:15 +0200

    Bruk sykmeldinger-backend i GCP

--- src/query-hooks/useSykmeldinger.ts
+++ src/query-hooks/useSykmeldinger.ts
@@ -1,13 +1,13 @@
 import { useQuery } from 'react-query'
 
 import { Sykmelding } from '../types/sykmelding'
-import { sykmeldingerBackendProxyRoot } from '../utils/environment'
+import { sykmeldingerBackendRoot } from '../utils/environment'
 import Fetch from '../utils/fetch'
 
 export default function UseSykmeldinger() {
     return useQuery<Sykmelding[], Error>('sykmeldinger', () =>
         Fetch.authenticatedGet(
-            `${sykmeldingerBackendProxyRoot()}/api/v1/sykmeldinger`,
+            `${sykmeldingerBackendRoot()}/api/v1/sykmeldinger`,
             async (maybeSykmeldinger) => {
                 return maybeSykmeldinger as Sykmelding[]
             }
