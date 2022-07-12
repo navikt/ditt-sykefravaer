540745af7009f19179fbf85ec849701190295741
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-07-12 11:49:14 +0200

    Revert "Endret banner"
    This reverts commit 501fe11b36a5feaccd6d4621b8e8b189363abd3c.

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
-import { Heading } from '@navikt/ds-react'
 
 interface BannerProps {
     children: React.ReactNode
 }
 
 const Banner = ({ children }: BannerProps) => {
-    return (
-        <header className="sidebanner">
-            <div className="limit">
-                <img
-                    className="sidebanner__ikon"
-                    src="/syk/sykefravaer/static/sykepenger.svg"
-                    alt="Sykepenger"
-                />
-                <Heading size="large" level="1" className="sidebanner__tittel">
-                    {children}
-                </Heading>
-            </div>
-        </header>
-    )
+    return <header className="sidebanner">{children}</header>
 }
 
 export default Banner
