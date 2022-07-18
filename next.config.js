b8d26c82b78cfc0f9eee58d8647938d09201f5f9
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-07-18 10:12:17 +0200

    Cache control på api

--- next.config.js
+++ next.config.js
@@ -67,7 +67,7 @@ module.exports = withPlugins(
                     headers: cspHeader,
                 },
                 {
-                    source: '/:path*',
+                    source: '/api/:path*',
                     headers: [
                         {
                             key: 'Cache-Control',
