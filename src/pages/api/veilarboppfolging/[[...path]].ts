f0dbade8da9ec02dd9f40a6f0759bdf0fadf7585
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-08-29 07:53:54 +0200

    proxy api til veilarboppfolging

---
+++ src/pages/api/veilarboppfolging/[[...path]].ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import getConfig from 'next/config'
+
+import { beskyttetApi } from '../../../auth/beskyttetApi'
+import { proxyKallTilBackend } from '../../../proxy/backendproxy'
+
+const { serverRuntimeConfig } = getConfig()
+
+const tillatteApier = ['GET /veilarboppfolging/api/v2/oppfolging']
+
+const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) => {
+    await proxyKallTilBackend({
+        req,
+        res,
+        tillatteApier,
+        backend: 'veilarboppfolging',
+        backendHostname: serverRuntimeConfig.veilarboppfolgingHost,
+        backendClientId: serverRuntimeConfig.veilarboppfolgingClientId,
+        https: true,
+    })
+})
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    },
+}
+
+export default handler
