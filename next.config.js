97110dc5ec4ee853cc6088238ca5bb2ce94f0ef7
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-09-30 07:48:33 +0200

    Ikke bruk compose plugin (#203)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withLess = require('next-with-less')
 const { withSentryConfig } = require('@sentry/nextjs')
-const withPlugins = require('next-compose-plugins')
 
 const csp = {
     'default-src': ["'none'"],
@@ -51,77 +50,76 @@ const cspHeader = [
     },
 ]
 
-module.exports = withPlugins(
-    [
-        [withLess],
-        [
-            (nextConfig) =>
-                process.env.ENABLE_SENTRY
-                    ? withSentryConfig(nextConfig, {
-                          silent: true,
-                      })
-                    : nextConfig,
-        ],
-    ],
-    {
-        async headers() {
-            return [
-                {
-                    source: '/:path*',
-                    headers: cspHeader,
-                },
-                {
-                    source: '/api/:path*',
-                    headers: [
-                        {
-                            key: 'Cache-Control',
-                            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
-                        },
-                    ],
-                },
-            ]
-        },
-        basePath: '/syk/sykefravaer',
-        lessLoaderOptions: {},
-        assetPrefix: process.env.ASSET_PREFIX || '',
-        serverRuntimeConfig: {
-            // Will only be available on the server side
-            decoratorEnv: process.env.DECORATOR_ENV,
-            noDecorator: process.env.NO_DECORATOR,
-            tokenXWellKnownUrl: process.env.TOKEN_X_WELL_KNOWN_URL,
-            tokenXPrivateJwk: process.env.TOKEN_X_PRIVATE_JWK,
-            tokenXClientId: process.env.TOKEN_X_CLIENT_ID,
-            idportenClientId: process.env.IDPORTEN_CLIENT_ID,
-            idportenWellKnownUrl: process.env.IDPORTEN_WELL_KNOWN_URL,
-            dittSykefravaerBackendClientId: process.env.DITT_SYKEFRAVAER_BACKEND_CLIENT_ID,
-            spinnsynBackendClientId: process.env.SPINNSYN_BACKEND_CLIENT_ID,
-            sykmeldingerBackendClientId: process.env.SYKMELDINGER_BACKEND_CLIENT_ID,
-            sykepengesoknadBackendClientId: process.env.SYKEPENGESOKNAD_BACKEND_CLIENT_ID,
-            narmestelederClientId: process.env.NARMESTELEDER_CLIENT_ID,
-            isdialogmoteClientId: process.env.ISDIALOGMOTE_CLIENT_ID,
-            syfomotebehovClientId: process.env.SYFOMOTEBEHOV_CLIENT_ID,
-            syfomotebehovUrl: process.env.SYFOMOTEBEHOV_URL,
-            syfomotebehovHost: process.env.SYFOMOTEBEHOV_HOST,
-            veilarboppfolgingHost: process.env.VEILARBOPPFOLGING_HOST,
-            veilarboppfolgingClientId: process.env.VEILARBOPPFOLGING_CLIENT_ID,
-            syfooppfolgingsplanserviceHost: process.env.SYFOOPPFOLGINGSPLANSERVICE_HOST,
-            syfooppfolgingsplanserviceClientId: process.env.SYFOOPPFOLGINGSPLANSERVICE_CLIENT_ID,
-        },
-        publicRuntimeConfig: {
-            // Will be available on both server and client
-            mockBackend: process.env.MOCK_BACKEND,
-            opplaering: process.env.OPPLAERING,
-            sykefravaerUrl: process.env.SYKEFRAVAER_URL,
-            minSideUrl: process.env.MINSIDE_URL,
-            amplitudeEnabled: process.env.AMPLITUDE_ENABLED,
-            environment: process.env.ENVIRONMENT,
-            spinnsynFrontendInterne: process.env.SPINNSYN_FRONTEND_INTERNE,
-            sykepengesoknadUrl: process.env.SYKEPENGESOKNAD_URL,
-            spinnsynUrl: process.env.SPINNSYN_URL,
-            sykmeldingUrl: process.env.SYKMELDING_URL,
-            aktivitetsplanUrl: process.env.AKTIVITETSPLAN_URL,
-            oppfolgingsplanUrl: process.env.OPPFOLGINGSPLAN_URL,
-            dialogmoteUrl: process.env.DIALOGMOTE_URL,
-        },
-    }
-)
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: cspHeader,
+            },
+            {
+                source: '/api/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+                    },
+                ],
+            },
+        ]
+    },
+    basePath: '/syk/sykefravaer',
+    lessLoaderOptions: {},
+    assetPrefix: process.env.ASSET_PREFIX || undefined,
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        decoratorEnv: process.env.DECORATOR_ENV,
+        noDecorator: process.env.NO_DECORATOR,
+        tokenXWellKnownUrl: process.env.TOKEN_X_WELL_KNOWN_URL,
+        tokenXPrivateJwk: process.env.TOKEN_X_PRIVATE_JWK,
+        tokenXClientId: process.env.TOKEN_X_CLIENT_ID,
+        idportenClientId: process.env.IDPORTEN_CLIENT_ID,
+        idportenWellKnownUrl: process.env.IDPORTEN_WELL_KNOWN_URL,
+        dittSykefravaerBackendClientId: process.env.DITT_SYKEFRAVAER_BACKEND_CLIENT_ID,
+        spinnsynBackendClientId: process.env.SPINNSYN_BACKEND_CLIENT_ID,
+        sykmeldingerBackendClientId: process.env.SYKMELDINGER_BACKEND_CLIENT_ID,
+        sykepengesoknadBackendClientId: process.env.SYKEPENGESOKNAD_BACKEND_CLIENT_ID,
+        narmestelederClientId: process.env.NARMESTELEDER_CLIENT_ID,
+        isdialogmoteClientId: process.env.ISDIALOGMOTE_CLIENT_ID,
+        syfomotebehovClientId: process.env.SYFOMOTEBEHOV_CLIENT_ID,
+        syfomotebehovUrl: process.env.SYFOMOTEBEHOV_URL,
+        syfomotebehovHost: process.env.SYFOMOTEBEHOV_HOST,
+        veilarboppfolgingHost: process.env.VEILARBOPPFOLGING_HOST,
+        veilarboppfolgingClientId: process.env.VEILARBOPPFOLGING_CLIENT_ID,
+        syfooppfolgingsplanserviceHost: process.env.SYFOOPPFOLGINGSPLANSERVICE_HOST,
+        syfooppfolgingsplanserviceClientId: process.env.SYFOOPPFOLGINGSPLANSERVICE_CLIENT_ID,
+    },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        mockBackend: process.env.MOCK_BACKEND,
+        opplaering: process.env.OPPLAERING,
+        sykefravaerUrl: process.env.SYKEFRAVAER_URL,
+        minSideUrl: process.env.MINSIDE_URL,
+        amplitudeEnabled: process.env.AMPLITUDE_ENABLED,
+        environment: process.env.ENVIRONMENT,
+        spinnsynFrontendInterne: process.env.SPINNSYN_FRONTEND_INTERNE,
+        sykepengesoknadUrl: process.env.SYKEPENGESOKNAD_URL,
+        spinnsynUrl: process.env.SPINNSYN_URL,
+        sykmeldingUrl: process.env.SYKMELDING_URL,
+        aktivitetsplanUrl: process.env.AKTIVITETSPLAN_URL,
+        oppfolgingsplanUrl: process.env.OPPFOLGINGSPLAN_URL,
+        dialogmoteUrl: process.env.DIALOGMOTE_URL,
+    },
+}
+
+const withSentry = (nextConfig) =>
+    process.env.ENABLE_SENTRY
+        ? withSentryConfig(nextConfig, {
+              silent: true,
+          })
+        : nextConfig
+
+module.exports = withLess(withSentry(nextConfig))
