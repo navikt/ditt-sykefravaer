421892d739f3d2631b9f25907d526212c480ee19
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-10-10 14:22:48 +0200

    Oppdaterer alle tester

--- cypress/e2e/alle_tester.cy.ts
+++ cypress/e2e/alle_tester.cy.ts
@@ -1,6 +1,6 @@
 import './run-0/forside.cy'
 import './run-0/heltFrisk.cy'
+import './run-1/arbeidssituasjon.cy'
 import './run-1/inntektsmelding.cy'
-import './run-1/narmesteleder.cy'
 import './run-2/oppfolgingsplanOppgaver.cy'
 import './run-3/snartSluttOppgave.cy'
