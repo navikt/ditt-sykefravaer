5c4c91d3cf613644a6a022acd27510eca401b771
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-02-20 13:18:56 +0100

    Bruk amplitude fra dekoratøren (#514)

--- src/components/amplitude/amplitude.ts
+++ src/components/amplitude/amplitude.ts
@@ -1,46 +1,21 @@
-import { AmplitudeClient } from 'amplitude-js'
+import { logAmplitudeEvent } from '@navikt/nav-dekoratoren-moduler'
+import { logger } from '@navikt/next-logger'
 
 import { amplitudeEnabled } from '../../utils/environment'
 
-interface AmplitudeInstance {
-    logEvent: (eventName: string, data?: Record<string, string>) => void
-}
-
-let amplitudeInstance: AmplitudeInstance | undefined
-
-const getLogEventFunction = (): AmplitudeInstance => {
-    if (window && amplitudeEnabled()) {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const amplitudeJs = require('amplitude-js')
-        const amplitudeInstance: AmplitudeClient = amplitudeJs.default.getInstance()
-        amplitudeInstance.init('default', '', {
-            apiEndpoint: 'amplitude.nav.no/collect-auto',
-            saveEvents: true,
-            includeUtm: true,
-            includeReferrer: true,
-            platform: window.location.toString(),
-            batchEvents: false,
-        })
-        return amplitudeInstance
-    } else {
-        return {
-            logEvent: (eventName: string, data?: Record<string, string>) => {
-                // eslint-disable-next-line no-console
-                console.log(`Logger ${eventName} - Event properties: ${JSON.stringify(data)}!`)
-            },
-        }
-    }
-}
-
 export type validEventNames = 'navigere' | 'alert vist' | 'knapp klikket' //Bruk kun navn fra taksonomien
 
-export const logEvent = (eventName: validEventNames, eventProperties: Record<string, string>) => {
+export const logEvent = (eventName: validEventNames, eventData: Record<string, string | boolean>) => {
     if (window) {
-        if (amplitudeInstance) {
-            amplitudeInstance.logEvent(eventName, eventProperties)
+        if (amplitudeEnabled()) {
+            logAmplitudeEvent({
+                origin: 'ditt-sykefravaer',
+                eventName,
+                eventData,
+            }).catch((e) => logger.warn(`Feil ved amplitude logging`, e))
         } else {
-            amplitudeInstance = getLogEventFunction()
-            amplitudeInstance.logEvent(eventName, eventProperties)
+            // eslint-disable-next-line no-console
+            console.log(`Logger ${eventName} - Event properties: ${JSON.stringify(eventData)}!`)
         }
     }
 }
