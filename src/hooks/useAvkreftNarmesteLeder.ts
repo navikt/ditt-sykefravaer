d5f72415b7bcb2fe9b46a40aac822df5896898f2
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-08-15 14:15:51 +0200

    Refaktor mock api

--- src/hooks/useAvkreftNarmesteLeder.ts
+++ src/hooks/useAvkreftNarmesteLeder.ts
@@ -2,12 +2,15 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { fetchMedRequestId } from '../utils/fetch'
 
+import { UseTestpersonQuery } from './useTestpersonQuery'
+
 export default function UseAvkreftNarmesteLeder(org: string) {
     const queryClient = useQueryClient()
+    const testpersonQuery = UseTestpersonQuery()
 
     return useMutation<unknown, Error>(
         () =>
-            fetchMedRequestId(`/syk/sykefravaer/api/narmesteleder/v2/${org}/avkreft`, {
+            fetchMedRequestId(`/syk/sykefravaer/api/narmesteleder/v2/${org}/avkreft` + testpersonQuery.query(), {
                 method: 'POST',
                 credentials: 'include',
                 headers: {
