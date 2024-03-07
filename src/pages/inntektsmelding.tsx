90018cb1d7322570bc05a574e952c60eaff1cff1
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-03-07 11:10:17 +0100

    Nye tekster

--- src/pages/inntektsmelding.tsx
+++ src/pages/inntektsmelding.tsx
@@ -1,12 +1,11 @@
 import { BodyLong, Heading, Panel } from '@navikt/ds-react'
 import React from 'react'
 
-import { parserWithReplace } from '../utils/html-react-parser-utils'
 import { beskyttetSideUtenProps } from '../auth/beskyttetSide'
 import { useUpdateBreadcrumbs } from '../hooks/useBreadcrumbs'
-import { tekst } from '../utils/tekster'
 import { Banner } from '../components/banner/Banner'
 import { Flexjar } from '../components/flexjar/flexjar'
+import { LenkeMedAmplitude } from '../components/lenke/lenke-med-amplitude'
 
 const Inntektsmelding = () => {
     useUpdateBreadcrumbs(
@@ -16,37 +15,72 @@ const Inntektsmelding = () => {
 
     return (
         <>
-            <Banner utenIkon={true} tittel={tekst('inntektsmelding.side-tittel-manglende')} />
+            <Banner utenIkon={true} tittel="Vi må bruke lenger tid på å behandle søknaden din" />
 
-            <Heading level="2" size="medium">
-                {tekst('inntektsmelding.side-tittel', {
-                    '%ARBEIDSGIVER%': 'Test Arbeidsgiver AS',
-                })}
+            <Heading level="2" size="medium" spacing>
+                Vi mangler inntektsmelding fra arbeidsgiveren din
             </Heading>
 
-            <BodyLong spacing>{parserWithReplace(tekst('inntektsmelding.arbeidsgiver-har-fatt-beskjed'))}</BodyLong>
+            <BodyLong spacing>
+                Vi har dessverre ikke mottatt inntektsmelding fra arbeidsgiveren din, vi trenger denne for å behandle
+                søknaden din om sykepenger. Arbeidsgiveren din skal ha blitt varslet om at vi trenger inntektsmelding.
+                Om du får betalt lønn som vanlig av din arbeidsgiver mens du er sykmeldt, er dette ikke noe du trenger å
+                gjøre noe med.
+            </BodyLong>
 
             <Panel className="my-8 bg-blue-50">
                 <Heading level="2" size="small">
-                    {tekst('inntektsmelding.soknaden-kan-ikke-behandles.tittel')}
+                    Hvorfor er dette viktig?
                 </Heading>
-                <BodyLong>{parserWithReplace(tekst('inntektsmelding.soknaden-kan-ikke-behandles'))}</BodyLong>
+                <BodyLong>
+                    Uten inntektsmelding kan vi ikke behandle søknaden din om sykepenger, og du kan ikke få utbetalt
+                    sykepenger. Vi trenger inntektsmelding for å vurdere retten din til sykepenger og eventuelt
+                    størrelsen på sykepengene.
+                </BodyLong>
             </Panel>
 
             <Panel className="my-8 bg-blue-50">
                 <Heading level="2" size="small">
-                    {tekst('inntekstmelding.hvorfor.varsler.vi.tittel')}
+                    Hva kan du gjøre?
                 </Heading>
-                <BodyLong spacing>{parserWithReplace(tekst('inntekstmelding.hvorfor.varsler.vi1'))}</BodyLong>
-                <BodyLong>{parserWithReplace(tekst('inntekstmelding.hvorfor.varsler.vi2'))}</BodyLong>
+                <BodyLong spacing>
+                    Ta kontakt med arbeidsgiveren din og be om at de sender inntektsmeldingen til NAV snarest mulig.
+                </BodyLong>
+                <BodyLong>
+                    {'Du kan også finne mer informasjon om inntektsmelding og saksbehandlingstid på '}
+                    <LenkeMedAmplitude
+                        url="https://www.nav.no/sykepenger#utbetaling"
+                        tekst="nav.no/sykepenger"
+                    ></LenkeMedAmplitude>
+                </BodyLong>
             </Panel>
 
             <Panel className="my-8 bg-blue-50">
                 <Heading level="2" size="small">
-                    {tekst('inntektsmelding.hva-er-inntekysmeldingen-tittel')}
+                    Hvor lang tid tar det å behandle søknaden?
                 </Heading>
-                <BodyLong>{tekst('inntektsmelding.hva-er-inntekysmeldingen-forklaring')}</BodyLong>
+                <BodyLong>
+                    Saksbehandlingstiden starter når vi har mottatt både søknaden din og inntektsmeldingen fra
+                    arbeidsgiveren. Forventet saksbehandlingstid kan være opp til fire uker etter at arbeidsgiveren din
+                    har sendt inn inntektsmelding.
+                </BodyLong>
             </Panel>
+
+            <Panel className="my-8 bg-blue-50">
+                <Heading level="2" size="small">
+                    Har du spørsmål?
+                </Heading>
+                <BodyLong>
+                    {'Du kan finne mer informasjon på '}
+                    <LenkeMedAmplitude
+                        url="https://www.nav.no/sykepenger"
+                        tekst="nav.no/sykepenger"
+                    ></LenkeMedAmplitude>
+                    {' eller du kan kontakte NAV kundeservice.'}
+                </BodyLong>
+            </Panel>
+
+            <BodyLong>Takk for din forståelse!</BodyLong>
 
             <Flexjar feedbackId="manglende-inntektsmelding" />
         </>
