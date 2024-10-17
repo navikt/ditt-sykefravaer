6601ed5a7b4012e6af7143708b45d3e02afa9885
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-10-17 12:40:10 +0200

    wip

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        exclude: ['**/node_modules/**', '**/playwrightTests/**'],
+        exclude: ['**/node_modules/**', '**/playwright/**'],
     },
 })
