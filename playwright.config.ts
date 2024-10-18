bdc59b4d1271e11ebb70bed255c5a7b00d3a4066
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-10-18 12:37:39 +0200

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-                navigationTimeout: 60000, // Navigation-specific timeout (60 seconds)
+        navigationTimeout: 60000, // Navigation-specific timeout (60 seconds)
 
         /* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'http://127.0.0.1:3000',
