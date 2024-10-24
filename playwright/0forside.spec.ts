880a8c74f22899f96e913eccebd7ee80fc6b8b6f
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-10-24 10:40:20 +0200

    wip

--- playwright/0forside.spec.ts
+++ playwright/0forside.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { checkA11y } from 'axe-playwright';
+// import { checkA11y } from 'axe-playwright';
 
 test.describe('Tester visning av forside', () => {
   test.beforeEach(async ({ page }) => {
@@ -11,9 +11,10 @@ test.describe('Tester visning av forside', () => {
     await expect(page.locator('text=Du har en ny søknad om sykepenger')).toBeVisible();
 
     // New assertion to check for the image
+      // TODO remove the line below
     const image = page.locator('img[src="https://picsum.photos/200/300"]');
     await expect(image).toBeVisible();
 
-    await checkA11y(page);
+   // await checkA11y(page);
   });
 });
