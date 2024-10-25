c17ca95ca41e353e60660a47ab16ba14baa73ec1
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-10-25 13:52:58 +0200

    this works (in that it creates errors)

--- playwright/0forside.spec.ts
+++ playwright/0forside.spec.ts
@@ -1,29 +1,19 @@
 import { test, expect } from '@playwright/test'
 // import { checkA11y } from 'axe-playwright';
-const { AxeBuilder } = require('@axe-core/playwright');
-const playwright = require('playwright');
-
+const { AxeBuilder } = require('@axe-core/playwright')
+const playwright = require('playwright')
 
 test.describe('Tester visning av forside', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('http://localhost:8080/syk/sykefravaer')
 
-
-        await page.waitForLoadState('networkidle'); // axe misses some accessibility issues if we don't wait for networkidle
-    const results = await new AxeBuilder({ page }).analyze();
-    // try {
-        expect(results.violations.length).toBe(0);
-    // } catch (e) {
-    //     console.log(results.violations);
-    //     throw new Error(`${results.violations.length} accessibility violations found.`);
-    // }
-
-
+        await page.waitForLoadState('networkidle') // axe misses some accessibility issues if we don't wait for networkidle
+        const results = await new AxeBuilder({ page }).analyze()
+        expect(results.violations.length).toBe(0)
     })
 
     test('Laster startside', async ({ page }) => {
         await expect(page).toHaveURL('http://localhost:8080/syk/sykefravaer')
         await expect(page.locator('text=Du har en ny søknad om sykepenger')).toBeVisible()
-
     })
 })
