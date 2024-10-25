1425f5b5233f0dae65b837cd1fb1681e33d56d95
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-10-25 15:08:51 +0200

    format

--- playwright/0forside.spec.ts
+++ playwright/0forside.spec.ts
@@ -1,15 +1,14 @@
 // example.spec.ts
-import { test } from './fixtures';
+import { test } from './fixtures'
 import { expect } from '@playwright/test'
 
-
 test.describe('Tester visning av forside', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:8080/syk/sykefravaer');
-  });
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:8080/syk/sykefravaer')
+    })
 
-  test('Laster startside', async ({ page }) => {
-    await expect(page).toHaveURL('http://localhost:8080/syk/sykefravaer');
-    await expect(page.locator('text=Du har en ny søknad om sykepenger')).toBeVisible();
-  });
-});
+    test('Laster startside', async ({ page }) => {
+        await expect(page).toHaveURL('http://localhost:8080/syk/sykefravaer')
+        await expect(page.locator('text=Du har en ny søknad om sykepenger')).toBeVisible()
+    })
+})
