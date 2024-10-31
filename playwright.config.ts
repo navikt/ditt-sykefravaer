d258d1851ce14a3a115b439ebb7fad081ed9c141
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-10-31 14:31:45 +0100

    Pw config tuning (#2027)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,65 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/test'
+
+const PORT = process.env.PORT || 8080
+
+type OptionsType = {
+    baseURL: string
+    timeout: number
+    server: PlaywrightTestConfig['webServer'] | undefined
+}
+
+const createOptions = (): OptionsType => {
+    const baseURL = `http://localhost:${PORT}`
+    const timeout = process.env.CI ? 30 * 1000 : 120 * 2 * 1000
+
+    if (process.env.CI) {
+        const prebuild = false
+        if (prebuild) {
+            return {
+                baseURL: `http://localhost:8080`,
+                timeout: 30 * 1000,
+                server: undefined,
+            }
+        }
+        return {
+            baseURL,
+            timeout,
+            server: {
+                command: 'npm run dev-ingen-dekorator',
+                port: 8080,
+                timeout: 120 * 1000, // Wait up to 2 minutes for the server to start
+                reuseExistingServer: false,
+            },
+        }
+    }
+
+    if (process.env.FAST) {
+        return {
+            baseURL,
+            timeout: 30 * 1000,
+            server: {
+                command: 'yarn start:e2e', //TODO fikse en fast lokal
+                url: baseURL,
+                timeout: 120 * 1000,
+                reuseExistingServer: false,
+            },
+        }
+    }
+
+    // Local dev server
+    return {
+        baseURL,
+        timeout,
+        server: {
+            command: 'npm run dev-ingen-dekorator',
+            port: 8080,
+            timeout: 120 * 1000, // Wait up to 2 minutes for the server to start
+            reuseExistingServer: true,
+        },
+    }
+}
+
+const opts = createOptions()
 
 export default defineConfig({
     testDir: './playwright',
@@ -9,6 +70,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: process.env.CI ? 'blob' : 'html',
     use: {
+        baseURL: opts.baseURL,
         navigationTimeout: 60000,
         trace: 'on-first-retry',
     },
@@ -22,10 +84,5 @@ export default defineConfig({
             use: { ...devices['Desktop Firefox'] },
         },
     ],
-    webServer: {
-        command: 'npm run dev-ingen-dekorator',
-        port: 8080,
-        timeout: 120 * 1000, // Wait up to 2 minutes for the server to start
-        reuseExistingServer: !process.env.CI,
-    },
+    webServer: opts.server,
 })
