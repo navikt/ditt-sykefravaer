ec3568adf875f6ca392962333749519b315c15b2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-11-06 13:51:54 +0100

    Fiks flaky tester og rask headless (#2034)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/test'
 
-const PORT = process.env.PORT || 3000
-
 type OptionsType = {
     baseURL: string
     timeout: number
@@ -9,27 +7,14 @@ type OptionsType = {
 }
 
 const createOptions = (): OptionsType => {
-    const baseURL = `http://localhost:${PORT}`
     const timeout = process.env.CI ? 30 * 1000 : 120 * 2 * 1000
 
+    const baseURL = `http://localhost:3000`
     if (process.env.CI) {
-        const prebuild = false
-        if (prebuild) {
-            return {
-                baseURL: `http://localhost:3000`,
-                timeout: 30 * 1000,
-                server: undefined,
-            }
-        }
         return {
             baseURL,
-            timeout,
-            server: {
-                command: 'npm run dev-ingen-dekorator',
-                port: 3000,
-                timeout: 120 * 1000, // Wait up to 2 minutes for the server to start
-                reuseExistingServer: false,
-            },
+            timeout: 30 * 1000,
+            server: undefined,
         }
     }
 
@@ -38,10 +23,12 @@ const createOptions = (): OptionsType => {
             baseURL,
             timeout: 30 * 1000,
             server: {
-                command: 'yarn start:e2e', //TODO fikse en fast lokal
-                url: baseURL,
+                command: 'npm run start',
+                port: 3000,
                 timeout: 120 * 1000,
                 reuseExistingServer: false,
+                stderr: 'pipe',
+                stdout: 'pipe',
             },
         }
     }
@@ -66,7 +53,7 @@ export default defineConfig({
     timeout: 30000,
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
+    retries: 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: process.env.CI ? 'blob' : 'html',
     use: {
