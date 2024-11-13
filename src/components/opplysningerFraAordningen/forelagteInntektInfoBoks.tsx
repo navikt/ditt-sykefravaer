b53f8ae23b487d0abb3242bd10bcba8c0712c13c
golshidbahadorian <golshid.bahadorian@nav.no>
2024-11-13 10:56:51 +0100

    Sorterer år og måneder så rekkefølgen er logisk

--- src/components/opplysningerFraAordningen/forelagteInntektInfoBoks.tsx
+++ src/components/opplysningerFraAordningen/forelagteInntektInfoBoks.tsx
@@ -8,6 +8,28 @@ interface InntektListeProps {
     grupperteInntekter: Record<string, Inntekt[]>
 }
 
+const MaanedsInntekt = ({ aar, maned, belop }: { aar: string; maned: string; belop: number | null }) => (
+    <div key={`${aar}-${maned}`}>
+        <BodyShort>
+            <BodyShort as="span" weight="semibold">
+                {capitalizeFirstLetter(getManedsNavn(maned))}:
+            </BodyShort>{' '}
+            {belop !== null ? `${formatterTall(belop)} kroner` : 'Ingen inntekt registrert'}
+        </BodyShort>
+    </div>
+)
+
+const InnhentetInntektForAar = ({ aar, maneder }: { aar: string; maneder: Inntekt[] }) => (
+    <div key={aar} className="mb-5">
+        <BodyShort weight="semibold">{aar}</BodyShort>
+        {maneder
+            .sort((a, b) => parseInt(a.maned) - parseInt(b.maned))
+            .map(({ maned, belop }) => (
+                <MaanedsInntekt key={maned} aar={aar} maned={maned} belop={belop} />
+            ))}
+    </div>
+)
+
 export const ForelagteInntektInfoBoks = ({ grupperteInntekter }: InntektListeProps) => {
     return (
         <Box padding="6" borderRadius="small" className="my-8 bg-gray-50">
@@ -29,21 +51,11 @@ export const ForelagteInntektInfoBoks = ({ grupperteInntekter }: InntektListePro
                 Inntekt hentet fra A-ordningen
             </Heading>
 
-            {Object.entries(grupperteInntekter).map(([aar, maneder]) => (
-                <div key={aar} className="mb-5">
-                    <BodyShort weight="semibold">{aar}</BodyShort>
-                    {maneder.map(({ maned, belop }) => (
-                        <div key={`${aar}-${maned}`}>
-                            <BodyShort>
-                                <BodyShort as="span" weight="semibold">
-                                    {capitalizeFirstLetter(getManedsNavn(maned))}:
-                                </BodyShort>{' '}
-                                {belop !== null ? `${formatterTall(belop)} kroner` : 'Ingen inntekt registrert'}
-                            </BodyShort>
-                        </div>
-                    ))}
-                </div>
-            ))}
+            {Object.entries(grupperteInntekter)
+                .sort(([aar1], [aar2]) => parseInt(aar1) - parseInt(aar2))
+                .map(([aar, maneder]) => (
+                    <InnhentetInntektForAar key={aar} aar={aar} maneder={maneder} />
+                ))}
 
             <BodyShort className="italic">Inntekten vist er før skatt</BodyShort>
         </Box>
