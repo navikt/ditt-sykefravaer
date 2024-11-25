03f014064779ace382535c23a320c49aad0ba863
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-11-25 09:40:59 +0100

    fikstest

--- src/components/Sykmelding/SykmeldingerSykmeldt/Nasjonal/AktivitetIkkeMulig.test.tsx
+++ src/components/Sykmelding/SykmeldingerSykmeldt/Nasjonal/AktivitetIkkeMulig.test.tsx
@@ -1,4 +1,11 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
+vi.mock('next/config', () => ({
+    default: () => ({
+        publicRuntimeConfig: {
+            someConfig: 'mockedValue',
+        },
+    }),
+}))
 
 import { AktivitetIkkeMuligPeriode, ArbeidsrelatertArsakType, MedisinskArsakType } from 'queries'
 
