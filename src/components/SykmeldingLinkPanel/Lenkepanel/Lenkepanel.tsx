900ca957aefb97435785af6e9b46ead7c0440b14
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-11-27 17:09:32 +0100

    less messy

--- src/components/SykmeldingLinkPanel/Lenkepanel/Lenkepanel.tsx
+++ src/components/SykmeldingLinkPanel/Lenkepanel/Lenkepanel.tsx
@@ -47,18 +47,17 @@ export function Lenkepanel({ sykmelding, notifying }: LenkepanelProps): ReactEle
                         <Heading size="small" level="3">
                             {getSykmeldingTitle(sykmelding)}
                         </Heading>
-                        <ul className="list-disc pl-4">
+                        <ul className="list-disc">
                             {sykmelding.sykmeldingsperioder.map((periode, index) => (
-                                <li key={index}>
-                                    <BodyShort className="overflow-anywhere">
-                                        {getDescription(periode, arbeidsgiverNavn)}
-                                    </BodyShort>
+                                <li key={index} className="pl-1.5 leading-relaxed">
+                                    {getDescription(periode, arbeidsgiverNavn)}
                                 </li>
                             ))}
                         </ul>
+
                     </div>
                     <div className="flex shrink-0 items-center">
-                        <LenkepanelEtikett status={status} behandlingsutfall={behandlingsutfallStatus} />
+                        <LenkepanelEtikett status={status} behandlingsutfall={behandlingsutfallStatus}/>
                     </div>
                 </div>
             </LinkPanel>
