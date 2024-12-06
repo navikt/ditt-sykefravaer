3258f941a23efca5fcbc4a8c48c46a51aba8d5f0
Golshid Bahadorian <36138378+golshidb93@users.noreply.github.com>
2024-12-06 14:01:57 +0100

    Justerer teksten og rekkefølge på ting basert på oppdatert skisse (#2133)

--- src/components/opplysningerFraAordningen/forelagteInntektInfoBoks.tsx
+++ src/components/opplysningerFraAordningen/forelagteInntektInfoBoks.tsx
@@ -1,4 +1,5 @@
-import { Box, BodyShort, Heading, BodyLong } from '@navikt/ds-react'
+import { Box, BodyShort, Heading, ReadMore } from '@navikt/ds-react'
+import React from 'react'
 
 import { capitalizeFirstLetter, getManedsNavn } from '../../utils/dato-utils'
 import { formatterTall } from '../../utils/tall-utils'
@@ -33,20 +34,6 @@ const InnhentetInntektForAar = ({ aar, maneder }: { aar: string; maneder: Inntek
 export const ForelagteInntektInfoBoks = ({ grupperteInntekter }: InntektListeProps) => {
     return (
         <Box padding="6" borderRadius="small" className="my-8 bg-gray-50">
-            <div className="border-b mb-8" style={{ borderColor: 'var(--a-grayalpha-500)' }}>
-                <Heading level="2" size="medium" spacing>
-                    Vi trenger at du sjekker om inntekten stemmer
-                </Heading>
-
-                <BodyLong spacing>
-                    Nav bruker vanligvis gjennomsnittet av inntekten din fra de siste 3 månedene før du ble syk for å
-                    beregne sykepengene dine. Hvis inntekten vi har hentet ikke stemmer med det du har tjent, må du gi
-                    beskjed så vi kan ta det med i beregningen.
-                </BodyLong>
-
-                <BodyLong spacing>Hvis inntekten stemmer og alt ser greit ut, trenger du ikke gjøre noe.</BodyLong>
-            </div>
-
             <Heading level="2" size="small" spacing>
                 Inntekt hentet fra a-ordningen
             </Heading>
@@ -58,6 +45,11 @@ export const ForelagteInntektInfoBoks = ({ grupperteInntekter }: InntektListePro
                 ))}
 
             <BodyShort className="italic">Inntekten vist er før skatt</BodyShort>
+
+            <ReadMore className="mt-4" header="Hva er a-ordningen?">
+                A-ordningen er et offentlig register hvor arbeidsgivere sender inn opplysninger om sine ansatte. Nav
+                bruker opplysninger fra dette registeret til å blant annet behandle søknader om sykepenger.
+            </ReadMore>
         </Box>
     )
 }
