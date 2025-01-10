cf97059c69c993073c766a274610432fbbf2a68a
oivindkulsrud <otk@fastmail.com>
2025-01-10 12:33:26 +0100

    Øker maks kjøretid for playwright tester (#2223)

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ const opts = createOptions()
 
 export default defineConfig({
     testDir: './playwright',
-    timeout: 15000,
+    timeout: 30000,
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: 0,
