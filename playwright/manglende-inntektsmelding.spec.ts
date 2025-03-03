2cefc82976bf26b4dd6eab3cd8189e3e8c39a8a6
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-03-03 16:07:15 +0100

    fungerer?

--- playwright/manglende-inntektsmelding.spec.ts
+++ playwright/manglende-inntektsmelding.spec.ts
@@ -9,12 +9,19 @@ test.describe('Tester inntektsmelding', () => {
         const oppgaver = page.getByTestId('oppgaver')
         const navdsAlert = oppgaver.locator('.navds-alert')
         await expect(navdsAlert).toHaveCount(1)
-        const alertText = 'Vi venter på inntektsmelding fra Flex AS.'
+        // const alertText = 'Vi venter på inntektsmelding fra Flex AS. Når vi får den kan vi behandle søknaden om sykepenger du sendte 1. juli 2022.' // 'Vi venter på inntektsmelding fra Flex AS.'
+        const alertText = 'Status i saken din om sykepenger: Vi venter på inntektsmelding fra Flex AS.'
         await expect(navdsAlert).toContainText(alertText)
 
+
+        // If there's a specific link inside the alert
+        await navdsAlert.locator('a').click()
+
+
         // todo fix link click in pupeteer (works in demo)
-        await page.goto('http://localhost:3000/syk/sykefravaer/inntektsmelding')
+        // await page.goto('http://localhost:3000/syk/sykefravaer/inntektsmelding')
         await expect(page).toHaveURL('http://localhost:3000/syk/sykefravaer/inntektsmelding')
+
 
         const header = page.locator('main').locator('h1').first()
         await expect(header).toBeVisible()
