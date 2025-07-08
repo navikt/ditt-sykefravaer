c785c4225c41264f03846a6f3f137f2c48446aff
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-07-08 17:26:30 +0200

    Fjerner test for å verifisere at uu testene kjøres automatisk

--- playwright/failAxe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from './utils/fixtures'
-
-test('skal feile axe-validering for knapp uten tilgjengelig navn', async ({ page }) => {
-    test.fail()
-    // Create a page with a button that has no text or aria-label
-    await page.setContent('<button></button>')
-})
