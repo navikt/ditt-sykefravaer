c4b7d123f30cbaeca6c004d54f9a0977918beb00
Eirik HS <eirik.skjaerseth@gmail.com>
2025-10-24 15:56:42 +0200

    forenkle Brukerinformasjon typer

--- src/hooks/useBrukerinformasjonById.ts
+++ src/hooks/useBrukerinformasjonById.ts
@@ -2,32 +2,23 @@ import { useQuery } from '@tanstack/react-query'
 
 import { fetchJsonMedRequestId } from '../utils/fetch'
 import { prettifyOrgName } from '../utils/orgUtils'
-import { Scalars } from '../fetching/graphql.generated'
 
 import { UseTestpersonQuery } from './useTestpersonQuery'
 
 export type NaermesteLeder = {
-    readonly __typename: 'NaermesteLeder'
-    readonly navn: Scalars['String']['output']
+    readonly navn: string
 }
 export type Arbeidsgiver = {
     readonly aktivtArbeidsforhold: boolean
     readonly naermesteLeder?: NaermesteLeder | null
     navn: string
-    readonly orgnummer: Scalars['String']['output']
+    readonly orgnummer: string
 }
 export type Brukerinformasjon = {
     readonly arbeidsgivere: ReadonlyArray<Arbeidsgiver>
 }
 export type BrukerinformasjonFragment = {
-    readonly __typename: 'Brukerinformasjon'
-    readonly arbeidsgivere: ReadonlyArray<{
-        readonly __typename: 'Arbeidsgiver'
-        readonly orgnummer: string
-        readonly navn: string
-        readonly aktivtArbeidsforhold: boolean
-        readonly naermesteLeder?: { readonly __typename: 'NaermesteLeder'; readonly navn: string } | null
-    }>
+    readonly arbeidsgivere: ReadonlyArray<Arbeidsgiver>
 }
 
 export default function useBrukerInformasjonById(sykmeldingId: string) {
