6e5ab881713c245994dc396599ad7048f4ec80a3
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-11-04 16:02:00 +0100

    Legg til tester for å validere korrekt dato i ulike tidssoner

--- playwright/sykmelding/general.spec.ts
+++ playwright/sykmelding/general.spec.ts
@@ -11,6 +11,61 @@ test.describe('sykmelding page tests that are not specific to a user', () => {
         await expect(page.getByRole('heading', { name: 'Opplysninger fra sykmeldingen' })).toBeVisible()
     })
 
+    test('burde vise korrekt dato uavhengig av tidssone', async ({ page, uuOptions }) => {
+        uuOptions.skipUU = true
+
+        await gotoScenario('normal')(page)
+        await navigateToFirstSykmelding('nye', '100%')(page)
+
+        await expect(page).toHaveURL('/syk/sykefravaer/sykmeldinger/id-apen-sykmelding')
+        const datePeriodElements = await page.getByText('8. - 15. januar 2025').all()
+
+        expect(datePeriodElements.length).toBeGreaterThan(0)
+
+        for (const element of datePeriodElements) {
+            await expect(element).toHaveText('8. - 15. januar 2025')
+        }
+    })
+
+    test('burde vise korrekt dato i UTC tidssone', async ({ browser, uuOptions }) => {
+        uuOptions.skipUU = true
+
+        const context = await browser.newContext({ timezoneId: 'UTC' })
+        const page = await context.newPage()
+
+        await gotoScenario('normal')(page)
+        await navigateToFirstSykmelding('nye', '100%')(page)
+
+        await expect(page).toHaveURL('/syk/sykefravaer/sykmeldinger/id-apen-sykmelding')
+        await expect(page).toHaveURL('/syk/sykefravaer/sykmeldinger/id-apen-sykmelding')
+        const datePeriodElements = await page.getByText('8. - 15. januar 2025').all()
+
+        expect(datePeriodElements.length).toBeGreaterThan(0)
+
+        for (const element of datePeriodElements) {
+            await expect(element).toHaveText('8. - 15. januar 2025')
+        }
+    })
+
+    test('burde vise korrekt dato i America/New_York tidssone', async ({ browser, uuOptions }) => {
+        uuOptions.skipUU = true
+
+        const context = await browser.newContext({ timezoneId: 'America/New_York' })
+        const page = await context.newPage()
+
+        await gotoScenario('normal')(page)
+        await navigateToFirstSykmelding('nye', '100%')(page)
+
+        await expect(page).toHaveURL('/syk/sykefravaer/sykmeldinger/id-apen-sykmelding')
+        const datePeriodElements = await page.getByText('8. - 15. januar 2025').all()
+
+        expect(datePeriodElements.length).toBeGreaterThan(0)
+
+        for (const element of datePeriodElements) {
+            await expect(element).toHaveText('8. - 15. januar 2025')
+        }
+    })
+
     test('legacy: a user without any "bruker svar" should still be able to view kvittering', async ({ page }) => {
         await gotoScenario('noBrukerSvar')(page)
         await navigateToFirstSykmelding('tidligere', '100%')(page)
