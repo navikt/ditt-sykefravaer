9a50abc6be00519c0fadd589e70fe8d2741f32fe
eirikhs <eirik.skjaerseth@gmail.com>
2025-11-11 14:06:38 +0100

    fjern unleash toggle sykmelding gradvis utrulling (#2846)

--- src/toggles/toggles.ts
+++ src/toggles/toggles.ts
@@ -4,5 +4,4 @@ export const EXPECTED_TOGGLES = [
     'ditt-sykefravaer-maxdato',
     'flexjar-ditt-sykefravaer-inntektsmelding-visning',
     'flexjar-sykmelding-kvittering',
-    'ditt-sykefravaer-sykmelding-gradvis-utrulling',
 ] as const
