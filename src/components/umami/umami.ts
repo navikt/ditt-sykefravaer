15684c000578b56a511ca39cf5394f2f40170595
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2025-11-28 15:48:04 +0100

    Ikke skru på Umami i prod inntil redact er på plass

--- src/components/umami/umami.ts
+++ src/components/umami/umami.ts
@@ -2,7 +2,7 @@ import { logAnalyticsEvent } from '@navikt/nav-dekoratoren-moduler'
 import { logger } from '@navikt/next-logger'
 import { useLayoutEffect, useRef } from 'react'
 
-import { umamiEnabled } from '../../utils/environment'
+import { isProd, umamiEnabled } from '../../utils/environment'
 
 export type validEventNames =
     | 'navigere'
@@ -20,7 +20,7 @@ export const logEvent = (eventName: validEventNames, eventData: Record<string, s
                 eventName,
                 eventData,
             }).catch((e) => logger.warn(`Feil ved umami logging`, e))
-        } else {
+        } else if (!isProd()) {
             // eslint-disable-next-line no-console
             console.log(`Logger ${eventName} - Event properties: ${JSON.stringify(eventData)}!`)
         }
