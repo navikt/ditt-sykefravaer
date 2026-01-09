77fec3b5f2e61ab05a625ef09512dc8164223a05
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-01-09 16:21:51 +0100

    Flytter aria-label for sykmeldinger i listevisning for å fikse WCAG 4.1.2

--- src/components/SykmeldingLinkPanel/Lenkepanel/Lenkepanel.tsx
+++ src/components/SykmeldingLinkPanel/Lenkepanel/Lenkepanel.tsx
@@ -5,8 +5,8 @@ import { BodyShort, Heading, LinkPanel } from '@navikt/ds-react'
 import { getReadableSykmeldingLength, getSykmeldingTitle } from '../../../utils/sykmeldingUtils'
 import { getDescription } from '../../../utils/periodeUtils'
 import { cn } from '../../../utils/tw-utils'
-import { toSykmeldingAriaLabel } from '../../../utils/toSykmeldingAriaLabel'
 import { Sykmelding } from '../../../types/sykmelding/sykmelding'
+import { toSykmeldingAriaLabel } from '../../../utils/toSykmeldingAriaLabel'
 
 import LenkepanelIcon from './LenkepanelIcon'
 import LenkepanelEtikett from './LenkepanelEtikett'
@@ -27,7 +27,12 @@ export function Lenkepanel({ sykmelding, notifying }: LenkepanelProps): ReactEle
     const sykmeldingPeriod: string = getReadableSykmeldingLength(sykmelding)
 
     return (
-        <Link href={`/sykmeldinger/${id}`} passHref legacyBehavior>
+        <Link
+            href={`/sykmeldinger/${id}`}
+            passHref
+            legacyBehavior
+            aria-label={toSykmeldingAriaLabel(sykmelding, sykmeldingPeriod)}
+        >
             <LinkPanel
                 className={cn('mb-4 p-6 [&>div]:w-full', {
                     'border-orange-300 bg-orange-50 hover:border-orange-500': notifying,
@@ -41,7 +46,7 @@ export function Lenkepanel({ sykmelding, notifying }: LenkepanelProps): ReactEle
                             isPaper={Boolean(papirsykmelding)}
                         />
                     </div>
-                    <div className="grow" aria-label={toSykmeldingAriaLabel(sykmelding, sykmeldingPeriod)}>
+                    <div className="grow">
                         <BodyShort>{sykmeldingPeriod}</BodyShort>
                         <Heading size="small" level="3">
                             {getSykmeldingTitle(sykmelding)}
