e3c634e54fa5bb83823fd3b77d30e2efe314560f
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-01-19 11:52:31 +0100

    Fjern siste rest av graphql

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,3 @@
-import path from 'node:path'
-
 import dotenv from 'dotenv'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
@@ -15,10 +13,5 @@ export default defineConfig({
         exclude: ['**/node_modules/**', '**/playwright/**'],
         include: ['src/**/*.test.{ts,tsx}'],
         setupFiles: ['vitest.setup.mts'],
-    },
-    resolve: {
-        alias: {
-            queries: path.resolve(__dirname, './src/fetching/graphql.generated.ts'),
-        },
     },
 })
